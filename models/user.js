ce99fa8866f72a754663c3d4af799f3993a5fd9f
Mithun Prajapati <prajapatimithun2151@gmail.com>
2026-02-11 12:22:48 +0530

    Final production-ready dependencies

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const passportLocalMongoose=require("passport-local-mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-const userSchema=new mongoose.Schema({
-    email:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
     }
-    //username and password is automatic added by passportLocalMongoose 
+    // username and password are automatically added by passportLocalMongoose
 });
 
-userSchema.plugin(passportLocalMongoose.default);
+userSchema.plugin(passportLocalMongoose);
 
-module.exports=mongoose.model("User",userSchema);
+module.exports = mongoose.model("User", userSchema);
